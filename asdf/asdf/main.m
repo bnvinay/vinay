a4ad2c6fe57848051ec7544a96c9c0e26f961266
User <user@Admins-Mac-mini.local>
2012-11-07 15:51:51 +0530

    "new"

--- asdf/asdf/main.m
+++ asdf/asdf/main.m
@@ -14,6 +14,7 @@ int main(int argc, const char * argv[])
     @autoreleasepool {
         
         // insert code here...
+        NSLog(@"vinay is here ");
         NSLog(@"Hello, World!");
         
     }
