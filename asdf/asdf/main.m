aee1661187c966da3fc4c32be20b6adc84794014
User <user@Admins-Mac-mini.local>
2012-11-07 15:47:53 +0530

    "commit"

---
+++ asdf/asdf/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  asdf
+//
+//  Created by User on 07/11/12.
+//  Copyright (c) 2012 User. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
