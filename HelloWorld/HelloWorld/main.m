8d4f259bb4811f8c370222bf2624980edcea6c3b
USER <dev@Admins-Mac-mini-2.local>
2012-11-09 10:48:36 +0530

    "HelloWorld pgm by Ganesh"

---
+++ HelloWorld/HelloWorld/main.m
@@ -0,0 +1,23 @@
+//
+//  main.m
+//  HelloWorld
+//
+//  Created by USER on 09/11/12.
+//  Copyright (c) 2012 USER. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        NSLog(@"Welcome To Regalix");
+        
+    }
+    return 0;
+}
+
